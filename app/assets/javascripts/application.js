bb54609bff0dbf45dc7117b4df132178c7f7f377
fsp712 <fainashalts@gmail.com>
2015-01-14 04:29:00 -0800

    fixed image issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,9 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(document).ready(function() {
-  $('.has-tooltip').tooltip();
-  $('.has-popover').popover({
-    trigger: 'hover'
-  });
-});
+// $(document).ready(function() {
+//   $('.has-tooltip').tooltip();
+//   $('.has-popover').popover({
+//     trigger: 'hover'
+//   });
+// });
